c6b30467147e3bdcc57a06f2321762961a680b28
DoZerrro <110924797+DoZerrro@users.noreply.github.com>
2022-08-24 01:22:10 +0900

    API문제2 완료

--- APIHomework/src/com/hw2/Run.java
+++ APIHomework/src/com/hw2/Run.java
@@ -1,4 +1,14 @@
 package com.hw2;
 
+import com.hw2.controller.SpaceUpper;
+
 public class Run {
+
+    //spaceToUpper() 메소드 실행
+    public static void main(String[] args) {
+        SpaceUpper spaceUpper = new SpaceUpper();
+
+        //SpaceUpper 클래스의 띄워쓰면 대문자로 바꿔주는 메소드 실행
+        spaceUpper.spaceToUpper();
+    }
 }
--- APIHomework/src/com/hw2/controller/SpaceUpper.java
+++ APIHomework/src/com/hw2/controller/SpaceUpper.java
@@ -1,4 +1,34 @@
 package com.hw2.controller;
 
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 public class SpaceUpper {
+
+    //기본 생성자
+    public SpaceUpper() {}
+    
+    //입력 받은 영문을 StringTokenizer와 StringBuilder를 사용해 띄워쓰기를 기준으로
+    //띄워쓰기 이후 첫 영문을 대문자로 출력하는 메소드
+    public void spaceToUpper() {
+        
+        //영문을 입력받는다.
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("영어를 입력하시오(띄워쓰기 포함) : ");
+        String inputStr = scanner.nextLine();
+
+        //StringTokenizer를 통해 띄워쓰기를 기준으로 토큰 단위로 나온 것을 연산하는 메소드를 구성
+        //띄워쓰기 이후 첫 글자는 대문자로 되게 하고 그 외에 나머진 소문자로 해서 StringBuilder에 담아 출력
+        StringTokenizer stringTokenizer = new StringTokenizer(inputStr, " ");
+        StringBuilder stringBuilder = new StringBuilder();
+
+        while(stringTokenizer.hasMoreTokens()) {
+            String token = stringTokenizer.nextToken();
+            stringBuilder.append(token.substring(0, 1).toUpperCase()+token.substring(1)+" ");
+        }
+
+        String resultStr = stringBuilder.toString();
+        System.out.println(resultStr);
+    }
 }
